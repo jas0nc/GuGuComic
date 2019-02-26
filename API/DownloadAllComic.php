7767354681d3db359caf33b34a311db64d979ee9
jas0nc <33709736+jas0nc@users.noreply.github.com>
2019-02-26 12:00:19 +0800

    Add files via upload

--- API/DownloadAllComic.php
+++ API/DownloadAllComic.php
@@ -222,11 +222,11 @@ foreach(array_reverse($ComicLinkArr) as $comic => $comicSN){
 							unlink($filename);
 						}
 					}
-				rmdir(__DIR__.'/../temp/'.$comic);
 				} 
 			} 
          }
      }
+	rmdir(__DIR__.'/../temp/'.$comic);
     echo 'Finish check :'.$comic.'
 ';
 }
